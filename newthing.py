4f4b9f590fdb0456953d59c17aa04dc13b6561d3
John K. Parejko <John.Parejko@alumni.carleton.edu>
2013-02-07 17:20:52 -0500

    Added some plotting of the really useful data.

--- newthing.py
+++ newthing.py
@@ -4,4 +4,9 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-data=np.loadtxt('data.dat')
+plt.ion()
+data=np.loadtxt('data/data.txt')
+
+xx = np.arange(0,1,0.1)
+plt.scatter(data[:,0],data[:,1],'*')
+plt.plot(xx,xx,lw=2)
